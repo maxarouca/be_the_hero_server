0c34cdc60ed42452dbc3598e340d2a32838acf85
Maximiler Arouca <maximiler@gmail.com>
2020-03-27 17:10:19 -0300

    :bug: Update a knex file
    Update a knex file

--- knexfile.js
+++ knexfile.js
@@ -13,34 +13,24 @@ module.exports = {
   },
 
   staging: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
+      filename: './src/database/db.sqlite',
     },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
     },
+    useNullAsDefault: true,
   },
 
   production: {
-    client: 'postgresql',
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
-    pool: {
-      min: 2,
-      max: 10,
+      filename: './src/database/db.sqlite',
     },
     migrations: {
-      tableName: 'knex_migrations',
+      directory: './src/database/migrations',
     },
+    useNullAsDefault: true,
   },
 }
